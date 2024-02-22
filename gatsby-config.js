9eddd427d4cf0b1e0e7f7835baa1311d63682127
Shaun Pezeshki <shaun@stickyagency.com>
2024-02-22 17:17:19 -0500

    Added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,10 +35,19 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        name: `analytics`,
-        trackingId: "G-JF4CNN7RZ6",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-JF4CNN7RZ6", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
     `gatsby-transformer-remark`,
