b67be02da4b9ad50987db2f2e2f4394136f9a0c7
Shaun Pezeshki <shaun@stickyagency.com>
2024-02-27 05:13:39 -0500

    Updated some css

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,15 @@ import { Link } from 'gatsby';
 
 const Header = ({ siteTitle, pageTitle }) => (
   <header className="container mx-auto p-4 flex flex-col md:flex-row justify-between border-b">
-    <div className="text-logo font-bold text-4xl mb-4 md:mb-0 md:text-left">
+    <div className="text-logo tracking-tight font-bold text-4xl mb-4 md:mb-0 md:text-left">
       <Link to="/" className="hover:underline">
         {siteTitle}
       </Link>
     </div>
     <nav className="menu w-full md:w-auto md:text-right pt-2">
       <ul className="flex flex-col md:flex-row md:space-x-10 text-xl">
-        <li><Link to="/about" activeClassName="underline" className="hover:underline font-medium">About Shaun</Link></li>
-        <li><Link to="/chat-with-shaun" activeClassName="underline" className="hover:underline font-medium">Chat With Shaun</Link></li>
+        <li><Link to="/about" activeClassName="underline" className="hover:underline font-medium tracking-tight">About Shaun</Link></li>
+        <li><Link to="/chat-with-shaun" activeClassName="underline" className="hover:underline font-medium tracking-tight">Chat With Shaun</Link></li>
       </ul>
     </nav>
   </header>
